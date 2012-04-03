39586d65168b608c66880073ef2181f4fa89d3a3
Fred Chien <cfsghost@gmail.com>
2012-04-03 12:28:29 +0800

    add login and applist page

--- app.js
+++ app.js
@@ -11,25 +11,28 @@ var app = module.exports = express.createServer();
 // Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 });
 
 // Routes
 
 app.get('/', routes.index);
+app.get('/login', routes.login);
+app.post('/login_verify', routes.login_verify);
+app.get('/applist', routes.applist);
 
 app.listen(80);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
+var AppHouse = require('AppHouse');
 
-/*
- * GET home page.
- */
+exports.index = function(req, res) {
+	res.render('index', { title: 'AppHouse Manager' })
+};
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+exports.login = function(req, res) {
+	res.render('login', { title: 'AppHouse Manager', err: req.query.err })
+};
+
+exports.login_verify = function(req, res) {
+	if (req.body.username == 'admin' && req.body.password == 'admin') {
+		res.redirect('/applist');
+	}
+
+	res.redirect('/login?err=1');
+};
+
+exports.applist = function(req, res) {
+	AppHouse.Manager.lsApp(function(apps) {
+		res.render('applist', { title: 'AppHouse Manager', apps: apps })
+	});
 };
